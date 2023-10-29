6c9f16025aa1975e9aea693321ea544ab1e0fb77
IUGAI1398 <hhhs2332@gmail.com>
2023-10-29 23:59:44 +0900

    initialize a supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://bjrghqdgxmvaobmwyike.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJqcmdocWRneG12YW9ibXd5aWtlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg1NzU1MjEsImV4cCI6MjAxNDE1MTUyMX0.6BUMR7Zqyt39ZsQ1jHJNoLBNJ3lQBnMjcJQwJW5cNVI"
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase;
